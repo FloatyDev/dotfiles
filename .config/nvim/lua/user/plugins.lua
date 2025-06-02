e77de3b3fd5a7e5f027a4b80f3756bbbe6b247bb
Giorgos Sygkelakis <ghsigel@hotmail.com>
2025-06-02 18:08:05 +0300

    feat: add markdown

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -241,5 +241,15 @@ require("lazy").setup({
 		config = function()
 			require("user._copilot")
 		end
-	}
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
+		ft = { "markdown", "codecompanion" },
+		opts = {
+			render_modes = true, -- Render in ALL modes
+			sign = {
+				enabled = false, -- Turn off in the status column
+			},
+		},
+	},
 })
